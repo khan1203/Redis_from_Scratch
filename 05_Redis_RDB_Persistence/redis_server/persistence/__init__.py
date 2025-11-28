44f941dc723d334452ea0be0cbf29d1129f556ea
khan1203 <rk.rifat1203@gmail.com>
2025-11-28 11:26:28 +0600

    code: RDB persistence integrated

---
+++ 05_Redis_RDB_Persistence/redis_server/persistence/__init__.py
@@ -0,0 +1,16 @@
+"""
+Redis Persistence Module
+
+This module provides persistence functionality for the Redis-like server including:
+- Append-Only File (AOF) logging
+- Redis Database (RDB) snapshots
+- Configuration management
+- Data recovery on startup
+"""
+
+from .config import PersistenceConfig
+from .rdb import RDBHandler
+from .recovery import RecoveryManager
+from .manager import PersistenceManager
+
+__all__ = ['PersistenceConfig', 'RDBHandler', 'RecoveryManager', 'PersistenceManager']
